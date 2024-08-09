60973800f7e3c58f4265bf5e080a16b2fa1dabee
omguddu100 <omguddu100@gmail.com>
2024-08-09 21:27:56 +0530

    updated loyout styling

--- src/app/modules/student-management/student-table/student-table.component.ts
+++ src/app/modules/student-management/student-table/student-table.component.ts
@@ -1,13 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+;
+import { DecimalPipe } from '@angular/common';
+interface Country {
+	name: string;
+	flag: string;
+	area: number;
+	population: number;
+}
 
+const COUNTRIES: Country[] = [
+	{
+		name: 'Russia',
+		flag: 'f/f3/Flag_of_Russia.svg',
+		area: 17075200,
+		population: 146989754,
+	},
+	{
+		name: 'Canada',
+		flag: 'c/cf/Flag_of_Canada.svg',
+		area: 9976140,
+		population: 36624199,
+	},
+	{
+		name: 'United States',
+		flag: 'a/a4/Flag_of_the_United_States.svg',
+		area: 9629091,
+		population: 324459463,
+	},
+	{
+		name: 'China',
+		flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+		area: 9596960,
+		population: 1409517397,
+	},
+];
 @Component({
   selector: 'app-student-table',
   templateUrl: './student-table.component.html',
   styleUrl: './student-table.component.scss',
 })
+
 export class StudentTableComponent implements OnInit {
   studentData: any;
-
+	countries = COUNTRIES;
   constructor() {}
   ngOnInit(): void {
     this.studentData = [
@@ -28,3 +63,13 @@ export class StudentTableComponent implements OnInit {
     ];
   }
 }
+
+
+
+
+
+
+
+// export class NgbdTableBasic {
+
+// }
