d305705de4ffce95a1e5d2bf4e26e0339ee87acd
meoutlook <nitishoutlook@gmail.com>
2024-08-11 01:11:19 +0530

    updated

--- src/app/modules/attendence-management/attendence-management.component.ts
+++ src/app/modules/attendence-management/attendence-management.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-attendence-management',
   templateUrl: './attendence-management.component.html',
-  styleUrl: './attendence-management.component.scss'
+  styleUrl: './attendence-management.component.scss',
 })
 export class AttendenceManagementComponent {
-
+  public routeObj = [
+    {
+      department_name: 'Guest Visitor Attendence',
+      action: 'Attendence_related',
+      link: 'guest-visitor-attendence',
+    },
+    {
+      department_name: 'Staff Attendence',
+      action: 'Attendence_related',
+      link: 'staff-attenndence',
+    },
+    {
+      department_name: 'Student Attendence',
+      action: 'Attendence_related',
+      link: 'students-attendence',
+    },
+    {
+      department_name: 'Teacher Attendence',
+      action: 'Attendence_related',
+      link: 'teacher-attendance',
+    },
+  ];
 }
--- src/app/modules/exam-details/exam-details.component.ts
+++ src/app/modules/exam-details/exam-details.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-exam-details',
   templateUrl: './exam-details.component.html',
-  styleUrl: './exam-details.component.scss'
+  styleUrl: './exam-details.component.scss',
 })
 export class ExamDetailsComponent {
-
+  public routeObj = [
+    {
+      department_name: 'Exam Result',
+      action: 'Exam_related',
+      link: 'exam-results',
+    },
+    {
+      department_name: 'Exam Sechdule',
+      action: 'Exam_related',
+      link: 'exam-sechdule',
+    },
+    {
+      department_name: 'Previous Paper',
+      action: 'Exam_related',
+      link: 'previous-paper',
+    },
+    {
+      department_name: 'Topper St. List',
+      action: 'Exam_related',
+      link: 'topper-student-list',
+    },
+  ];
 }
